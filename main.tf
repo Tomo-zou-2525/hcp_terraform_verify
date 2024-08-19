4f74836430bfe03c158ebaafa1e06f4ff799f3ad
Tomo-zou-2525 <bathbook1990tomozou@gmail.com>
2024-08-19 15:50:15 +0900

    initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.4"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.16"
+    }
+  }
+}
+
+resource "aws_vpc" "hcp_terraform_vpc_100725229921" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform_vpc_100725229921"
+  }
+}
+
+# AWSアカウント番号を取得
+data "aws_caller_identity" "current" {}
+# パーティション情報を取得
+data "aws_partition" "current" {}
+# リージョン情報を取得
+data "aws_region" "current" {}
